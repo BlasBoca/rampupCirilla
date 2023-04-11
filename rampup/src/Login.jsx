d6d339805997f5040bb816aeaaed6f17d493ab8f
BlasBoca <blas.krivocheev@example.com>
2023-04-11 16:07:55 +0200

    post login

--- rampup/src/Login.jsx
+++ rampup/src/Login.jsx
@@ -1,9 +1,53 @@
+import { useState } from 'react';
+
 /**
- * Login component
+ * This is the login component
  * @returns {JSX}
  */
 const Login = () => {
-    return <div> HERE GOES THE LOGIN </div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const cambioUsername = (event) => {
+    setUsername(event.target.value);
   };
-  
-  export { Login };
+
+  const cambioPassword = (event) => {
+    
+    const newPassword=event.target.value;
+    setPassword(newPassword);
+    //ahora digo q sea mayor a 7 caracteres
+
+    if(newPassword.length<=7){
+      setPasswordError('Password must be more than 7 letters');
+    }else{
+      setPasswordError('');
+    }
+    
+  };
+
+  const Submit = (event) => {
+    event.preventDefault();
+    if (username.trim() === '' || password.trim() === '') {
+      // .trim borra espacios vacios
+      return;
+    }
+  };
+
+  return (
+    <form onSubmit={Submit}>
+      <label>
+        Username: <input type="text" value={username} onChange={cambioUsername}></input>
+      </label>
+      <label>
+        Password: <input type="password" value={password} onChange={cambioPassword}></input>
+      </label>
+      {passwordError}
+      <button type="submit">Send</button>
+      <button type="createNewAccount">Create new account</button>
+    </form>
+  );
+};
+
+export { Login };
