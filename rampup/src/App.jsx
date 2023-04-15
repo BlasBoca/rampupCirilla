9acbb8637dc78621eec948d24c33b23088cb25f5
BlasBoca <blas.krivocheev@example.com>
2023-04-15 15:58:11 +0200

    parcial parte 3

--- rampup/src/App.jsx
+++ rampup/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
     { username: 'user2', password: 'pass2' },
   ]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [formData, setFormData] = useState({ name: '', lastName: '', age: '', id: '' });
+  const [savedData, setSavedData] = useState([]);
 
   const handleLogin = (formData) => {
     const matchingCredentials = validCredentials.filter(
@@ -33,6 +35,22 @@ function App() {
     setShowRegistration(true);
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSavedData([...savedData, formData]);
+    setFormData({ name: '', lastName: '', age: '', id: '' });
+  };
+
+  const handleDelete = (idToDelete) => {
+    const newData = savedData.filter((data) => data.id !== idToDelete);
+    setSavedData(newData);
+  };
+
   return (
     <div className="App">
       {!showRegistration && !isLoggedIn && (
@@ -50,7 +68,62 @@ function App() {
       )}
 
       {isLoggedIn && (
+        <div>
         <h1>Welcome, you are now logged in</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Name:</label>
+            <input 
+              type='text'
+              id='name'
+              name='name'
+              value={formData.name}
+              onChange={handleChange}
+              />
+          </div>
+          <div>
+          <label htmlFor="lastName">Last name:</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="age">Age:</label>
+              <input
+                type="number"
+                id="age"
+                name="age"
+                value={formData.age}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="id">ID:</label>
+              <input
+                type="text"
+                id="id"
+                name="id"
+                value={formData.id}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">Send</button>
+          </form>
+          <div>
+          {savedData.map((data) => (
+              <div key={data.id}>
+                <span>Name: {data.name} </span>
+                <span>Last Name: {data.lastName} </span>
+                <span>Age: {data.age}</span>
+                </div>
+            ))}
+          </div>
+          <button onClick={() => setIsLoggedIn(false)}>Log out</button>
+        </div>
       )}
     </div>
   );
